b4982b024e9df2729370ab1e7f4d067b669debb8
chaowangcanada <chaowang.ubc@gmail.com>
2017-11-27 23:40:34 -0500

    Modify Webapp Controller

--- webapp/src/main/java/controller/FavoriteController.java
+++ webapp/src/main/java/controller/FavoriteController.java
@@ -7,49 +7,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * Created by chao on 2017-11-24.
+ */
+@Path("/favorite")
+@Produces(MediaType.APPLICATION_JSON)
 public class FavoriteController {
 
-    /**
-     * Created by chao on 2017-11-24.
-     */
-    @Path("/favorite")
-    @Produces(MediaType.APPLICATION_JSON)
-    public class UserController {
-        @Autowired
-        private FavoriteDao favoriteDao;
+    @Autowired
+    private FavoriteDao favoriteDao;
 
-        @GET
-        @Path("/all")
-        public Response getAllFavorite(){
-            return new Response (1, null, favoriteDao.findAll());
+    @GET
+    @Path("/all")
+    public Response getAllFavorite(){
+        return new Response (1, null, favoriteDao.findAll());
+    }
+
+    @GET
+    @Path("/findbyuidcustom")
+    public Response getFavoriteByUidCustom(@QueryParam("uid") int uid){
+        return new Response (1, null, favoriteDao.findByUid(uid));
+    }
+
+    @GET
+    @Path("/findbyuid/{uid}")
+    public Response getFavoriteByUid(@PathParam("uid") int uid){
+        return new Response(1, null, favoriteDao.findByUid(uid));
+    }
+
+    @POST
+    @Path("/create")
+    public Response createFavorite(FavoriteParam favoriteParam){
+        if (favoriteParam.getUid() <= 0 || favoriteParam.getPropertyId() == null){
+            return new Response(0, "The user id or property ID couldn't be null", null);
         }
 
-        @GET
-        @Path("/findbyuidcustom")
-        public Response getFavoriteByUidCustom(@QueryParam("uid") int uid){
-            return new Response (1, null, favoriteDao.findByUid(uid));
-        }
-
-        @GET
-        @Path("/findbyuid/{uid}")
-        public Response getFavoriteByUid(@PathParam("uid") int uid){
-            return new Response(1, null, favoriteDao.findByUid(uid));
-        }
-
-        @POST
-        @Path("/create")
-        public Response createFavorite(FavoriteParam favoriteParam){
-            if (favoriteParam.getUid() <= 0 || favoriteParam.getPropertyId() == null){
-                return new Response(0, "The user id or property ID couldn't be null", null);
-            }
-
-            FavoriteEntity newFavorite = new FavoriteEntity();
-            newFavorite.setUid(favoriteParam.getUid());
-            newFavorite.setPropertyId(favoriteParam.getPropertyId());
-            favoriteDao.save(newFavorite);
-            return new Response(1, "Create succeeded", null);
-        }
-
+        FavoriteEntity newFavorite = new FavoriteEntity();
+        newFavorite.setUid(favoriteParam.getUid());
+        newFavorite.setPropertyId(favoriteParam.getPropertyId());
+        favoriteDao.save(newFavorite);
+        return new Response(1, "Create succeeded", null);
     }
 
 }
+
